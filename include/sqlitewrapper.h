73923114b15b2170e9d8631638de834b12698df3
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2018-01-04 11:09:35 -0800

    add statement bind, step, and column methods

--- include/sqlitewrapper.h
+++ include/sqlitewrapper.h
@@ -5,6 +5,8 @@
 #include <common/error.h>
 #include <stdint.h>
 
+#include <string>
+
 namespace sqlite {
 
 class statement;
@@ -17,9 +19,22 @@ error_code_args
 
    error_code_args(int rc_) : db(nullptr), rc(rc_) {}
 
+   error_code_args(sqlite3 *db_) : db(db_), rc(SQLITE_ERROR) {get_db_rc();}
+
    error_code_args(sqlite3 *db_, int rc_) : db(db_), rc(rc_) {}
 
    error_code_args(sqlite3_stmt *stmt_, int rc_) : db(sqlite3_db_handle(stmt_)), rc(rc_) {}
+
+   error_code_args(sqlite3_stmt *stmt_) : db(sqlite3_db_handle(stmt_)), rc(SQLITE_ERROR) {get_db_rc();}
+
+private:
+   void
+   get_db_rc()
+   {
+      int rc2;
+      if (db && (rc2 = sqlite3_errcode(db)))
+         rc = rc2;
+   }
 };
 
 void
@@ -74,6 +89,9 @@ class statement
    void
    check_open(error *err);
 
+   void
+   check_column(int idx, error *err);
+
 public:
    statement();
    statement(const statement &other) = delete;
@@ -82,6 +100,9 @@ public:
    void close();
 
    bool step(error *err);
+
+   int
+   bind_count();
 
    void
    bind(int idx, error *err);
@@ -100,6 +121,73 @@ public:
 
    void
    bind(int idx, double d, error *err);
+
+   int
+   column_count();
+
+   const char *
+   column_name(int idx, error *err);
+
+   int
+   column_type(int idx, error *err);
+
+   void
+   column(int idx, int64_t &i, error *err);
+
+   void
+   column(int idx, double &d, error *err);
+
+   void
+   column(int idx, const char *&string, size_t &len, error *err);
+
+   void
+   column(int idx, std::string &str, error *err);
+
+   void
+   column(int idx, const void *&blob, size_t &len, error *err);
+
+   void
+   column(int idx, uint64_t &i, error *err)
+   {
+      int64_t t = 0;
+      column(idx, t, err);
+      i = t;
+   }
+
+   int64_t
+   column_int(int idx, error *err)
+   {
+      int64_t i = 0;
+      column(idx, i, err);
+      return i;
+   }
+
+   double
+   column_double(int idx, error *err)
+   {
+      double d = 0.0;
+      column(idx, d, err);
+      return d;
+   }
+
+   const char *
+   column_cstr(int idx, error *err)
+   {
+      const char *str = nullptr;
+      size_t discard = 0;
+
+      column(idx, str, discard, err);
+
+      return str;
+   }
+
+   std::string
+   column_str(int idx, error *err)
+   {
+      std::string s;
+      column(idx, s, err);
+      return s;
+   }
 };
 
 } // end namespace
--- src/stmt.cc
+++ src/stmt.cc
@@ -1,4 +1,5 @@
 #include <sqlitewrapper.h>
+#include <string.h>
 
 sqlite::statement::statement()
    : stmt(nullptr)
@@ -26,6 +27,54 @@ sqlite::statement::check_open(error *err)
    if (!stmt)
       ERROR_SET(err, unknown, "Method called on null statement object");
 exit:;
+}
+
+void
+sqlite::statement::check_column(int idx, error *err)
+{
+   // We could check stmt separately, but this method gives a better error
+   // message.
+   //
+   check_open(err);
+   ERROR_CHECK(err);
+
+   if (idx < 0 || idx >= sqlite3_column_count(stmt))
+      ERROR_SET(err, unknown, "Column index out of bounds");
+exit:;
+}
+
+bool
+sqlite::statement::step(error *err)
+{
+   int rc = 0;
+   bool r = false;
+
+   check_open(err);
+   ERROR_CHECK(err);
+
+   rc = sqlite3_step(stmt);
+   switch (rc)
+   {
+   case SQLITE_ROW:
+      r = true;
+      break;
+   case SQLITE_DONE:
+      break;
+   default:
+      ERROR_SET(err, sqlite, error_code_args(stmt, rc));
+   }
+
+exit:
+   return r;
+}
+
+int
+sqlite::statement::bind_count()
+{
+   int r = 0;
+   if (stmt)
+      r = sqlite3_bind_parameter_count(stmt);
+   return r;
 }
 
 void
@@ -115,5 +164,134 @@ sqlite::statement::bind(int idx, double d, error *err)
 
    if ((rc = sqlite3_bind_double(stmt, idx+1, d)))
       ERROR_SET(err, sqlite, error_code_args(stmt, rc));
+exit:;
+}
+
+int
+sqlite::statement::column_count()
+{
+   int r = 0;
+   if (stmt)
+      r = sqlite3_column_count(stmt);
+   return r;
+}
+
+const char *
+sqlite::statement::column_name(int idx, error *err)
+{
+   const char *r = nullptr;
+
+   check_column(idx, err);
+
+   r = sqlite3_column_name(stmt, idx);
+   if (!r)
+      ERROR_SET(err, sqlite, error_code_args(stmt));
+
+exit:
+   return r;
+}
+
+
+int
+sqlite::statement::column_type(int idx, error *err)
+{
+   int r = 0;
+
+   check_column(idx, err);
+   ERROR_CHECK(err);
+
+   r = sqlite3_column_type(stmt, idx);
+exit:
+   return r;
+}
+
+void
+sqlite::statement::column(int idx, int64_t &i, error *err)
+{
+   check_column(idx, err);
+   ERROR_CHECK(err);
+
+   i = sqlite3_column_int64(stmt, idx);
+exit:;
+}
+
+void
+sqlite::statement::column(int idx, double &d, error *err)
+{
+   check_column(idx, err);
+   ERROR_CHECK(err);
+
+   d = sqlite3_column_double(stmt, idx);
+exit:;
+}
+
+void
+sqlite::statement::column(int idx, const char *&string, size_t &len, error *err)
+{
+   const unsigned char *r = nullptr;
+   int n = 0;
+
+   check_column(idx, err);
+   ERROR_CHECK(err);
+
+   if (sqlite3_column_type(stmt, idx) == SQLITE_NULL)
+   {
+      string = nullptr;
+      len = 0;
+      goto exit;
+   }
+
+   //
+   // Per sqlite documentation, the right way to do this is to get the text
+   // pointer first, then ask about length.
+   //
+
+   r = sqlite3_column_text(stmt, idx);
+   if (!r)
+      ERROR_SET(err, sqlite, error_code_args(stmt));
+   string = (const char*)r;
+   n = sqlite3_column_bytes(stmt, idx);
+exit:;
+}
+
+void
+sqlite::statement::column(int idx, const void *&blob, size_t &len, error *err)
+{
+   check_column(idx, err);
+   ERROR_CHECK(err);
+
+   if (sqlite3_column_type(stmt, idx) == SQLITE_NULL)
+   {
+      blob = nullptr;
+      len = 0;
+      goto exit;
+   }
+
+   blob = sqlite3_column_blob(stmt, idx);
+   if (!blob)
+      ERROR_SET(err, sqlite, error_code_args(stmt));
+   len = sqlite3_column_bytes(stmt, idx);
+
+exit:;
+}
+
+void
+sqlite::statement::column(int idx, std::string &str, error *err)
+{
+   try
+   {
+      const char *cstr = nullptr;
+      size_t len = 0;
+
+      column(idx, cstr, len, err);
+      ERROR_CHECK(err);
+
+      str.resize(0);
+      str.append(cstr, len);
+   }
+   catch (std::bad_alloc)
+   {
+      ERROR_SET(err, errno, ENOMEM);
+   }
 exit:;
 }
